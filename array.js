393de23062876e082f8b5e5550cf7ed9f393bcdd
litonp <litondas7121@gmail.com>
2023-10-07 12:41:19 +0600

    first

---
+++ array.js
@@ -0,0 +1,7 @@
+const country = 'Bangladesh';
+const age = 53;
+const isIndependent = true;
+const student = { id: 121, class: 12, name: 'Agun' };
+const friends = [20, 21, 22, 23, 24, 25, 26];
+
+console.log(typeof country);

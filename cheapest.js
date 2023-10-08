d810dead6e7a2533c528c71a7764c4b13c653cc5
litonp <litondas7121@gmail.com>
2023-10-08 22:15:21 +0600

    phones

--- cheapest.js
+++ cheapest.js
@@ -1,3 +1,25 @@
+// const phones = [
+//     { name: 'Samsung', cemera: 12, storage: '32gb', price: 36000, color: 'silver' },
+//     { name: 'Walton', cemera: 10, storage: '32gb', price: 22000, color: 'silver' },
+//     { name: 'iphone', cemera: 8, storage: '32gb', price: 82000, color: 'silver' },
+//     { name: 'Xaomi', cemera: 10, storage: '32gb', price: 52000, color: 'silver' },
+//     { name: 'Oppo', cemera: 10, storage: '32gb', price: 20000, color: 'silver' },
+// ];
+
+// function cheapestPhone(phones) {
+//     let cheapest = phones[0];
+//     for (let i = 0; i < phones.length; i++) {
+//         const phone = phones[i];
+//         if (phone.cemera < cheapest.cemera) {
+//             cheapest = phone;
+//         }
+//     }
+//     return cheapest;
+// }
+
+// const mySelection = cheapestPhone(phones);
+// console.log(mySelection);
+
 const phones = [
     { name: 'Samsung', cemera: 12, storage: '32gb', price: 36000, color: 'silver' },
     { name: 'Walton', cemera: 10, storage: '32gb', price: 22000, color: 'silver' },

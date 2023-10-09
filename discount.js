9daad62eee74ce1e152e93f01340c81041f79578
litonp <litondas7121@gmail.com>
2023-10-09 17:20:49 +0600

    dis

---
+++ discount.js
@@ -0,0 +1,36 @@
+/*
+1. if ticket numbers is less than 100, per ticket price: 100
+2. if ticket numbers is less than 100, but less than 200. First 100 tickets will be 100/ ticket rest tickets will be 90 taka per price per ticket price: 100
+3. if you purchase more than 200 ticketw
+   frist 100 ---> 100tk
+   101-200 ---> 90tk
+   200+ ---> 70tk
+*/
+
+function ticketPrice(ticketQuantity) {
+    const first100Rate = 100;
+    const second100Rate = 90;
+    const restTicketPrice = 70;
+    if (ticketQuantity <= 100) {
+        const price = ticketQuantity * first100Rate;
+        return price;
+    }
+    else if (ticketQuantity <= 200) {
+        const first100Price = 100 * first100Rate;
+        const restTicketQuantity = ticketQuantity - 100;
+        const restTicketPrice = restTicketQuantity * second100Rate;
+        const totalPrice = first100Price + restTicketPrice;
+        return totalPrice;
+    }
+    else {
+        const first100Price = 100 * first100Rate;
+        const second100Price = 100 * second100Rate;
+        const restTicketQuantity = ticketQuantity - 200;
+        const restTicketPrice = restTicketQuantity * restTicketRate;
+        const totalCoat = first100Price + second100Price + restTicketPrice;
+        return totalCoat;
+    }
+}
+
+const price = ticketPrice(220);
+console.log(price);
